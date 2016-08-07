91219846ded3fcaa468b0da3b821e88ffdf2ff88
Lixin Yang <lsrob@hotmail.com>
2016-08-07 14:21:15 +1000

    h

--- LxUtilities.Services/Persistence/Ef/EfModelMapRegister.cs
+++ LxUtilities.Services/Persistence/Ef/EfModelMapRegister.cs
@@ -1,13 +1,28 @@
-﻿using AutoMapper;
-using LxUtilities.Definitions.Mapping;
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using LxUtilities.Definitions.Core.Domain.Entity;
+using LxUtilities.Definitions.Persistence;
+using LxUtilities.Services.Mapping.AutoMapper;
 
 namespace LxUtilities.Services.Persistence.EF
 {
-    public class EfModelMapRegister : IMapRegister
+    public abstract class EfModelMapRegisterBase : MapRegisterBase
     {
-        public void RegisterMaps()
+        public override void RegisterMaps()
         {
-            Mapper.Initialize(config=>config.CreateMap());
+            Mapper.Initialize(config =>
+            {
+            });
+        }
+
+        protected virtual void AddEfModelMaps<TEntity, TRelationalModel>(
+            IDictionary<TEntity, TRelationalModel> mapItems)
+            where TEntity : class, IEntity
+            where TRelationalModel : IRelationalModel<TEntity>;
+
+        {
+            
         }
     }
 }
