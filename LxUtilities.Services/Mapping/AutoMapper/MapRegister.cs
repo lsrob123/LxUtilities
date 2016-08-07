703406be6ee41ea7103f3a87f2cf993f1a8ef25a
Lixin Yang <lsrob@hotmail.com>
2016-08-07 13:51:48 +1000

    h

---
+++ LxUtilities.Services/Mapping/AutoMapper/MapRegister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LxUtilities.Definitions.Mapping;
+
+namespace LxUtilities.Services.Mapping.AutoMapper
+{
+    public class MapRegister
+    {
+        protected ICollection<Action<IMapperConfigurationExpression>> CreateMapActions;
+
+        public MapRegister(params Action<IMapperConfigurationExpression>[] createMapActions)
+        {
+        }
+
+
+        public void RegisterMaps()
+        {
+            Mapper.Initialize(config => config.CreateMap());
+        }
+    }
+}
