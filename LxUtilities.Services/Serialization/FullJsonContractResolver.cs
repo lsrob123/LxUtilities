30b0d5ae8d2cf9cfcd3c947eeca65da4769ab1ff
Lixin Yang <lsrob@hotmail.com>
2016-08-09 09:16:08 +1000

    t

---
+++ LxUtilities.Services/Serialization/FullJsonContractResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace LxUtilities.Services.Serialization
+{
+    public class FullJsonContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var props = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Select(p => CreateProperty(p, memberSerialization))
+                .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Select(f => CreateProperty(f, memberSerialization)))
+                .ToList();
+            props.ForEach(p =>
+            {
+                p.Writable = true;
+                p.Readable = true;
+            });
+            return props;
+        }
+    }
+}
--- LxUtilities.Services/Serialization/JsonSerializer.cs
+++ LxUtilities.Services/Serialization/JsonSerializer.cs
@@ -5,16 +5,30 @@ namespace LxUtilities.Services.Serialization
 {
     public class JsonSerializer : ISerializer
     {
-        public string Serialize(object anyObject)
+        public string Serialize(object anyObject, bool useFullContractResolver = false)
         {
-            var jsonString = JsonConvert.SerializeObject(anyObject);
-            return jsonString;
+            if (!useFullContractResolver)
+            {
+                var s1 = JsonConvert.SerializeObject(anyObject);
+                return s1;
+            }
+
+            var setting = new JsonSerializerSettings {ContractResolver = new FullJsonContractResolver()};
+            var s2 = JsonConvert.SerializeObject(anyObject, setting);
+            return s2;
         }
 
-        public T Deserialize<T>(string serialized)
+        public T Deserialize<T>(string serialized, bool useFullContractResolver = true)
         {
-            var objectFromJson = JsonConvert.DeserializeObject<T>(serialized);
-            return objectFromJson;
+            if (!useFullContractResolver)
+            {
+                var o1 = JsonConvert.DeserializeObject<T>(serialized);
+                return o1;
+            }
+
+            var setting = new JsonSerializerSettings {ContractResolver = new FullJsonContractResolver()};
+            var o2 = JsonConvert.DeserializeObject<T>(serialized, setting);
+            return o2;
         }
     }
 }
