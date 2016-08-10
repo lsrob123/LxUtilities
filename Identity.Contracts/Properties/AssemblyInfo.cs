5a0c9b0083c2c7eb58a49679a176fe92fd8b2934
Lixin Yang <lsrob@hotmail.com>
2016-08-10 21:09:22 +1000

    no message

---
+++ Identity.Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Identity.Contracts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Identity.Contracts")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97127388-0563-4d34-b27b-d9bb1f26b143")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LxUtilities.Definitions/Persistence/RelationalModelBase.cs
@@ -0,0 +1,12 @@
+﻿namespace LxUtilities.Definitions.Persistence
+{
+    public abstract class RelationalModelBase : IRelationalModel
+    {
+        public long Id { get; protected set; }
+
+        public virtual void SetId(long id)
+        {
+            Id = id;
+        }
+    }
+}
